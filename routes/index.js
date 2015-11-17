323eb914feb4c9e274aad6591b6c1f2df56ee6a7
octavianrotari <octavianrotari@gmail.com>
2015-11-17 18:01:53 +0000

    (WIP) sends email by using env variable

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+
 
 router.get('/', function(req, res) {
   res.render('index', {});
@@ -24,5 +28,38 @@ router.get('/flotta', function(req, res) {
 
 router.get('/contatti', function(req, res) {
   res.render('contatti', {});
+});
+
+router.post('/send_mail', function(req, res){
+  var mailOpts , smtpTrans;
+
+  smtpTrans = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+      user: 'octavianrotari@gmail.com',
+      pass: process.env.PASS.replace(':','')
+    }
+  });
+
+  mailOpts = {
+    from: req.body.nome + " " + req.body.email,
+    to: 'octavianrotari@gmail.com',
+    subject: 'contact form sito Autotrasporti',
+    text: req.body.testo
+  };
+
+  console.log(mailOpts.from);
+
+  console.log(mailOpts);
+
+  smtpTrans.sendMail (mailOpts, function(error, message){
+    if (error) {
+      console.log(error);
+      res.render('contatti', { title: 'Error', msg: "error", err: true, page: 'contatti'});
+    }
+    else {
+      res.render('contatti', { title: 'successo', msg: "email spedita", err: false, page: "contatti"});
+    }
+  });
 });
 module.exports = router;
