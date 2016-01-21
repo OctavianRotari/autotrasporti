9101c3491b5e2639af5e4624d66ff0cafacbd30d
octavianrotari@gmail.com <octavianrotari@gmail.com>
2016-01-21 16:08:27 +0000

    maps ref

--- public/javascripts/slide.js
+++ public/javascripts/slide.js
@@ -1,35 +1,37 @@
+$(document).ready(function() {
 
-$(function(){
+  $(function(){
 
-  var width = 720;
-  var animationSpeed = 3000;
-  var pause = 1000;
-  var currentSlide = 1;
+    var width = 720;
+    var animationSpeed = 3000;
+    var pause = 1000;
+    var currentSlide = 1;
 
-  var $slider = $('#slider');
-  var $slideContainer = $slider.find(".slides");
-  var $slides = $slideContainer.find(".slide");
+    var $slider = $('#slider');
+    var $slideContainer = $slider.find(".slides");
+    var $slides = $slideContainer.find(".slide");
 
-  var interval;
+    var interval;
 
-  function startSlider (){
-    interval = setInterval(function(){
+    function startSlider (){
+      interval = setInterval(function(){
         $slideContainer.animate({'margin-left': '-=' + width}, animationSpeed, function(){
           currentSlide++;
           if (currentSlide === $slides.length){
-              currentSlide = 1;
-              $slideContainer.css('margin-left', 0);
+            currentSlide = 1;
+            $slideContainer.css('margin-left', 0);
           }
         });
-    }, pause);
-  }
+      }, pause);
+    }
 
-  function stopSlider (){
-    clearInterval(interval);
-  }
+    function stopSlider (){
+      clearInterval(interval);
+    }
 
 
-  $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
 
-  startSlider();
+    startSlider();
+  });
 });
